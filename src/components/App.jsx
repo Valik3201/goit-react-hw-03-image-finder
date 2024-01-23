47ffb9ae93998a9080aa9b9b12ba381b64bcf5e3
Valik <valik3201@gmail.com>
2024-01-23 23:20:25 +0100

    feat(ui): add navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Searchbar } from './Searchbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Image Finder
+    <div>
+      <Searchbar />
     </div>
   );
 };
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import {
+  Button,
+  Container,
+  Col,
+  Row,
+  Form,
+  Navbar,
+  ButtonGroup,
+  ToggleButton,
+} from 'react-bootstrap';
+
+export const Searchbar = () => {
+  const [radioValue, setRadioValue] = useState('light');
+
+  const radios = [
+    { name: 'Light', value: 'light' },
+    { name: 'Dark', value: 'dark' },
+  ];
+
+  const handleThemeChange = theme => {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  };
+
+  return (
+    <Navbar fixed="top">
+      <Container className="justify-content-center">
+        <Row className="mb-2 mb-md-0 justify-content-between align-items-center w-100">
+          <Navbar.Brand as={Col} xs={2} href="#">
+            Image Finder
+          </Navbar.Brand>
+
+          <ButtonGroup
+            as={Col}
+            xs={2}
+            md={{ order: 'last' }}
+            className="mb-2 mb-md-0 d-flex justify-content-end"
+          >
+            {radios.map((radio, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`theme-radio-${idx}`}
+                type="radio"
+                variant="secondary"
+                name="theme"
+                value={radio.value}
+                checked={radioValue === radio.value}
+                onChange={e => {
+                  setRadioValue(e.currentTarget.value);
+                  handleThemeChange(e.currentTarget.value);
+                }}
+              >
+                {radio.name}
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
+
+          <Form className="d-flex" as={Col} xs={12} md={6}>
+            <Form.Control
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="primary">Search</Button>
+          </Form>
+        </Row>
+      </Container>
+    </Navbar>
+  );
+};
