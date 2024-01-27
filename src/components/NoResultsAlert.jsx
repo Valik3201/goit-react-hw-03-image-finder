440bf7005145c3def9293dd4de329e1c17989d3e
Valik <valik3201@gmail.com>
2024-01-27 17:16:37 +0100

    feat(ui): add alerts for zero search results

---
+++ src/components/NoResultsAlert.jsx
@@ -0,0 +1,23 @@
+import { Container, Row, Col, Alert } from 'react-bootstrap';
+
+function NoResultsAlert({ searchQuery }) {
+  return (
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={6}>
+          <Alert variant="warning">
+            <Alert.Heading>Oops!</Alert.Heading>
+            <p>
+              We couldn't find any results for{' '}
+              <span className="fw-bold">{searchQuery}</span>.
+            </p>
+            <hr />
+            <p className="mb-0">Please try a different search term.</p>
+          </Alert>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default NoResultsAlert;
